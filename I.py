16ffebb47541812015a2bc468cae764af9c32715
Alex Chindris <chindrisalex@yahoo.ro>
2022-05-08 20:47:03 +0300

    Am facut partea de gasire a unei solutii; ramane de facut input si output dupa cerinte

---
+++ I.py
@@ -0,0 +1,33 @@
+import localsolver
+
+with localsolver.LocalSolver() as ls:
+    model = ls.model
+    d = 2
+    r = 3
+    SC = [80, 57]
+    D = [96, 22, 14]
+    C = [[81, 51, 31],
+         [82, 47, 54]]
+    X = [[model.int(0, max(SC)) for i in range(r)] for j in range(d)]
+    #  observatie: cu X = [[model etc] * r for i in range(d)] apar probleme, parca nu creeaza cate un element
+    #  distinct pt fiecare loc din lista
+    for k in range(r):
+        model.constraint(model.sum(X[j][k] for j in range(d)) == D[k])
+    for j in range(d):
+        model.constraint(model.sum(X[j][k] for k in range(r)) <= SC[j])
+    transport_cost = model.sum(model.sum(X[j][k] * C[j][k] for k in range(r)) for j in range(d))
+    # poate facut alte verificari pt a vedea daca expresia de la transport_cost e buna; eventual
+    # vazut ceva de genul daca apar erori la anumite modificari in expresie
+    model.minimize(transport_cost)
+    model.close()
+    ls.param.time_limit = 10
+    ls.solve()
+    for i in range(d):
+        for j in range(r):
+            print(X[i][j].value, end=" ")
+        print()
+    print(transport_cost.value)
+    # cu model.constraint cred ca se vor scrie restrictiile mentionate pe kb la pb clasica de transport.
+    # Ce am scris pana acum cu constraint: model.constraint(model.sum(X[j][k] for i in range())).
+    # E pentru prima din restrictiile mentionate pe kb si nu reprezinta complet acea restrictie.
+    # Vezi exemple de programe cu localsolver pentru a putea finaliza tema.
